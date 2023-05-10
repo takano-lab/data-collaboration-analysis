8a14cc64727b2a3f12125e6405db111bd3bccadd
sns-ikeda <ikeda.shunnosuke@gmail.com>
2023-05-11 00:51:25 +0900

    data_collaboration.pyのリファクタ

--- src/data_collaboration.py
+++ src/data_collaboration.py
@@ -1,217 +1,207 @@
-import tqdm
+from __future__ import annotations
+
+from typing import TypeVar
+
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import PCA, TruncatedSVD
+from sklearn.decomposition import TruncatedSVD
+from tqdm import tqdm
+
+from config.config import Config
+
+logger = TypeVar("logger")
 
 
 class DataCollaborationAnalysis:
-    def __init__(self, config, logger):
-        self.config = config
-        self.logger = logger
+    def __init__(self, config: Config, logger: logger, train_df: pd.DataFrame, test_df: pd.DataFrame) -> None:
+        self.config: Config = config
+        self.logger: logger = logger
 
-    def produce_anchor(self, train):
-        """
-        アンカーデータを生成する関数
-        """
-        np.random.seed(seed=self.config["seed"])
-        self.anchor = np.random.rand(self.config["num_anchor_data"], train.shape[1])
-        return self.anchor
+        # 本当はできるだけattributeを持たせない方が良い
+        # 元データ
+        self.train_df: pd.DataFrame = train_df
+        self.test_df: pd.DataFrame = test_df
+        self.anchor: np.ndarray = np.array([])
 
-    def split_data(self, train, test, train_rating, test_rating):
+        # 機関ごとの分割データ
+        self.Xs_train: list[np.ndarray] = []
+        self.Xs_test: list[np.ndarray] = []
+        self.ys_train: list[np.ndarray] = []
+        self.ys_test: list[np.ndarray] = []
+
+        # 中間表現
+        self.anchors_inter: list[np.ndarray] = []
+        self.Xs_train_inter: list[np.ndarray] = []
+        self.Xs_test_inter: list[np.ndarray] = []
+        self.ys_train_inter: list[np.ndarray] = []
+        self.ys_test_inter: list[np.ndarray] = []
+
+        # 統合表現
+        self.X_train_integ: np.ndarray = np.array([])
+        self.X_test_integ: np.ndarray = np.array([])
+        self.y_train_integ: np.ndarray = np.array([])
+        self.y_test_integ: np.ndarray = np.array([])
+
+    def run(self) -> None:
+        """
+        データ分割、中間表現の生成、統合表現の生成を一気に行う関数
+        """
+        # データの分割
+        self.Xs_train, self.Xs_test, self.ys_train, self.ys_test = self.train_test_split(
+            train_df=self.train_df,
+            test_df=self.test_df,
+            num_institution=self.config.num_institution,
+            num_institution_user=self.config.num_institution_user,
+        )
+
+        # アンカーデータの生成
+        self.anchor = self.produce_anchor(
+            num_row=self.config.num_anchor_data, num_col=self.Xs_train[0].shape[1], seed=self.config.seed
+        )
+
+        # 中間表現の生成
+        self.make_intermediate_expression()
+
+        # 統合表現の生成
+        self.make_integrate_expression()
+
+    @staticmethod
+    def train_test_split(
+        train_df: pd.DataFrame, test_df: pd.DataFrame, num_institution: int, num_institution_user: int
+    ) -> tuple[list[np.ndarray], list[np.ndarray], list[np.ndarray], list[np.ndarray]]:
         print("********************データの分割********************")
         """
         複数機関を想定してデータセットを分割する関数
         """
+        y_train_ser = train_df["rating"]
+        X_train_df = train_df.drop("rating", axis=1)
+        y_test_ser = test_df["rating"]
+        X_test_df = test_df.drop("rating", axis=1)
+
         # 格納しておくリスト
-        train_x_list = []
-        train_y_list = []
-        test_x_list = []
-        test_y_list = []
+        Xs_train, Xs_test = [], []
+        ys_train, ys_test = [], []
 
         # データセットを分割する
-        for institute_start in tqdm.tqdm(
+        for institute_start in tqdm(
             range(
                 0,
-                self.config["num_institution"] * self.config["num_institution_user"],
-                self.config["num_institution_user"],
+                num_institution * num_institution_user,
+                num_institution_user,
             )
         ):
             # 一時的に格納する変数
-            temp_train_x = pd.DataFrame()
-            temp_test_x = pd.DataFrame()
+            X_train_df_tmp, X_test_df_tmp = pd.DataFrame(), pd.DataFrame()
 
-            for user_id in range(
-                institute_start, institute_start + self.config["num_institution_user"]
-            ):
-                user_id += 1
-
+            for user_id in range(institute_start + 1, institute_start + num_institution_user + 1):  # user_idは1から始まる
                 # trainのuser_idが一致する箇所のindexを抽出
-                train_index = train[f"uid_{user_id}"] == 1
+                index_train = X_train_df[f"uid_{user_id}"] == 1
                 # testのuser_idが一致する箇所のindexを抽出
-                test_index = test[f"uid_{user_id}"] == 1
+                index_test = X_test_df[f"uid_{user_id}"] == 1
 
                 # train_index, test_indexに一致するデータを抽出
-                temp_train_x = pd.concat([temp_train_x, train[train_index]], axis=0)
-                temp_test_x = pd.concat([temp_test_x, test[test_index]], axis=0)
+                X_train_df_tmp = pd.concat([X_train_df_tmp, X_train_df[index_train]], axis=0)
+                X_test_df_tmp = pd.concat([X_test_df_tmp, X_test_df[index_test]], axis=0)
 
             # tempを1つのarrayに変換し、リストに格納
-            train_x_list.append(temp_train_x.values)
-            test_x_list.append(temp_test_x.values)
+            Xs_train.append(X_train_df_tmp.values)
+            Xs_test.append(X_test_df_tmp.values)
 
             # yはtemp_train_xに対応するratingを格納
-            train_y_list.append(train_rating[temp_train_x.index])
-            test_y_list.append(test_rating[temp_test_x.index])
+            ys_train.append(y_train_ser[X_train_df_tmp.index].values)
+            ys_test.append(y_test_ser[X_test_df_tmp.index].values)
 
-        print("機関の数: ", len(train_x_list))
+        print("機関の数: ", len(Xs_train))
 
-        return train_x_list, train_y_list, test_x_list, test_y_list
+        return Xs_train, Xs_test, ys_train, ys_test
 
-    def make_intermediate_expression(self, train_x_list, test_x_list, anchor):
+    @staticmethod
+    def produce_anchor(num_row: int, num_col: int, seed: int = 0) -> np.ndarray:
+        """
+        アンカーデータを生成する関数
+        """
+        np.random.seed(seed=seed)
+        anchor = np.random.rand(num_row, num_col)
+        return anchor
+
+    def make_intermediate_expression(self) -> None:
         print("********************中間表現の生成********************")
         """
         中間表現を生成する関数
         """
-        intermediate_train_x_list = []
-        intermediate_test_x_list = []
-        intermediate_anchor_list = []
-        for institute in tqdm.tqdm(range(self.config["num_institution"])):
+        for institute in tqdm(range(self.config.num_institution)):
             # 各機関の訓練データ, テストデータおよびアンカーデータを取得し、svdを適用
-            svd_train_x, svd_anchor, svd_test_x = self.svd(
-                train_x_list[institute],
-                anchor,
-                test_x_list[institute],
-                self.config["dim_intermediate"],
+            X_train_svd, X_test_svd, anchor_svd = DataCollaborationAnalysis.svd(
+                self.Xs_train[institute],
+                self.Xs_test[institute],
+                self.anchor,
+                self.config.dim_intermediate,
             )
             # svdを適用したデータをリストに格納
-            intermediate_train_x_list.append(svd_train_x)
-            intermediate_test_x_list.append(svd_test_x)
-            intermediate_anchor_list.append(svd_anchor)
+            self.Xs_train_inter.append(X_train_svd)
+            self.Xs_test_inter.append(X_test_svd)
+            self.anchors_inter.append(anchor_svd)
 
-        print("中間表現の次元数: ", intermediate_train_x_list[0].shape[1])
-        return (
-            intermediate_train_x_list,
-            intermediate_test_x_list,
-            intermediate_anchor_list,
-        )
+        print("中間表現の次元数: ", self.Xs_train_inter[0].shape[1])
 
-    def make_integrate_expression(
-        self,
-        intermediate_train_x_list,
-        intermediate_test_x_list,
-        intermediate_anchor_list,
-        train_y_list,
-        test_y_list,
-    ):
+    def make_integrate_expression(self) -> None:
         print("********************統合表現の生成********************")
         """
         統合表現を生成する関数
         """
         # アンカーデータを水平方向に開く（アンカーデータ数 × 各機関の中間表現次元の合計）
-        centralized_anchor = np.hstack(intermediate_anchor_list)
+        centralized_anchor = np.hstack(self.anchors_inter)  # \hat{X}^{anc}
 
         # 特異値分解（Uはアンカーデータ数 × 統合表現の次元数）
         U, _, _ = np.linalg.svd(centralized_anchor)
-        U = U[:, : self.config["dim_integrate"]]
+        U = U[:, : self.config.dim_integrate]  # 固有値の大きい順に統合表現の次元数だけ取得
+
         # Zは統合表現の次元数 × アンカーデータ数
         Z = U.T
 
         # 各機関の統合関数を求め、統合表現を生成
-        integrate_train_x_list = []
-        integrate_test_x_list = []
-
-        for institute in tqdm.tqdm(range(self.config["num_institution"])):
+        Xs_train_integrate, Xs_test_integrate = [], []
+        for institute in tqdm(range(self.config.num_institution)):
             # 各機関のアンカーデータの中間表現を転置して、擬似逆行列を求める
-            pseudo_inverse = np.linalg.pinv(intermediate_anchor_list[institute].T)
+            pseudo_inverse = np.linalg.pinv(self.anchors_inter[institute].T)  # \hat{X}^{anc}+
 
             # 各機関の統合関数を求める
-            integrate_function = np.dot(Z, pseudo_inverse)
+            integrate_function = np.dot(Z, pseudo_inverse)  # G^{(i)}
 
-            # 統合関数で各機関の中間表現を統合
-            institute_train_x_intermediate = intermediate_train_x_list[institute].T
-            institute_test_x_intermediate = intermediate_test_x_list[institute].T
-
-            institute_train_x_integrate = np.dot(
-                integrate_function, institute_train_x_intermediate
-            )
-            institute_test_x_integrate = np.dot(
-                integrate_function, institute_test_x_intermediate
-            )
+            # 統合関数で各機関の中間表現を統合表現に変換
+            X_train_integrate = np.dot(integrate_function, self.Xs_train_inter[institute].T)
+            X_test_integrate = np.dot(integrate_function, self.Xs_test_inter[institute].T)
 
             # 統合表現をリストに格納
-            integrate_train_x_list.append(institute_train_x_integrate.T)
-            integrate_test_x_list.append(institute_test_x_integrate.T)
+            Xs_train_integrate.append(X_train_integrate.T)
+            Xs_test_integrate.append(X_test_integrate.T)
 
-        print("統合表現の次元数: ", integrate_train_x_list[0].shape[1])
+        print("統合表現の次元数: ", Xs_train_integrate[0].shape[1])
 
         # 全ての機関の統合表現をくっつけ、1つのarrayに変換
-        integrate_train_x = np.vstack(integrate_train_x_list)
-        integrate_test_x = np.vstack(integrate_test_x_list)
+        self.X_train_integ = np.vstack(Xs_train_integrate)
+        self.X_test_integ = np.vstack(Xs_test_integrate)
 
         # yもくっつける
-        integrate_train_y = np.hstack(train_y_list)
-        integrate_test_y = np.hstack(test_y_list)
+        self.y_train_integ = np.hstack(self.ys_train)
+        self.y_test_integ = np.hstack(self.ys_test)
 
         # logにも出力
-        self.logger.info("統合表現（訓練データ）の数と次元数: {}".format(integrate_train_x.shape))
-        self.logger.info("統合表現（テストデータ）の数と次元数: {}".format(integrate_test_x.shape))
-        self.logger.info("統合表現（訓練データの正解）の数と次元数: {}".format(integrate_train_y.shape))
-        self.logger.info("統合表現（テストデータの正解）の数と次元数: {}".format(integrate_test_y.shape))
-
-        return integrate_train_x, integrate_test_x, integrate_train_y, integrate_test_y
-
-    def all_apply(self, train, test, train_rating, test_rating):
-        """
-        データ分割、中間表現の生成、統合表現の生成を一気に行う関数
-        """
-
-        # アンカーデータの生成
-        anchor = self.produce_anchor(train)
-
-        # データの分割
-        train_x_list, train_y_list, test_x_list, test_y_list = self.split_data(
-            train, test, train_rating, test_rating
-        )
-
-        # 中間表現の生成
-        (
-            intermediate_train_x_list,
-            intermediate_test_x_list,
-            intermediate_anchor_list,
-        ) = self.make_intermediate_expression(train_x_list, test_x_list, anchor)
-
-        # 統合表現の生成
-        (
-            integrate_train_x,
-            integrate_test_x,
-            integrate_train_y,
-            integrate_test_y,
-        ) = self.make_integrate_expression(
-            intermediate_train_x_list,
-            intermediate_test_x_list,
-            intermediate_anchor_list,
-            train_y_list,
-            test_y_list,
-        )
-
-        return (
-            train_x_list,
-            train_y_list,
-            test_x_list,
-            test_y_list,
-            integrate_train_x,
-            integrate_test_x,
-            integrate_train_y,
-            integrate_test_y,
-        )
+        self.logger.info(f"統合表現（訓練データ）の数と次元数: {self.X_train_integ.shape}")
+        self.logger.info(f"統合表現（テストデータ）の数と次元数: {self.X_test_integ.shape}")
+        self.logger.info(f"統合表現（訓練データの正解）の数と次元数: {self.y_train_integ.shape}")
+        self.logger.info(f"統合表現（テストデータの正解）の数と次元数: {self.y_test_integ.shape}")
 
     @staticmethod
-    def svd(train_x, anchor_x, test_x, n_components):
+    def svd(
+        X_train: np.ndarray, X_test: np.ndarray, anchor: np.ndarray, n_components: int
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         """
-        train_xを基準にsvdを適用し、train_x, anchor_x, test_xを次元削減する関数
+        X_trainを基準にsvdを適用し、X_train, X_test, anchorを次元削減する関数
         """
         svd = TruncatedSVD(n_components=n_components)
-        svd.fit(train_x)
-        svd_train_x = svd.transform(train_x)
-        svd_anchor_x = svd.transform(anchor_x)
-        svd_test_x = svd.transform(test_x)
-        return svd_train_x, svd_anchor_x, svd_test_x
+        svd.fit(X_train)
+        X_train_svd = svd.transform(X_train)
+        X_test_svd = svd.transform(X_test)
+        anchor_svd = svd.transform(anchor)
+        return X_train_svd, X_test_svd, anchor_svd
