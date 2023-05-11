b6f021131107c09ad75c038b82693bd883a1a9dc
sns-ikeda <ikeda.shunnosuke@gmail.com>
2023-05-11 19:11:25 +0900

    train_val.pyのリファクタ

--- src/train_val.py
+++ src/train_val.py
@@ -1,54 +1,69 @@
+from __future__ import annotations
+
+from typing import Optional, TypeVar
+
+import category_encoders as ce
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import TruncatedSVD
+
+from config.config import Config
 from src.model import run_lgbm, run_surprise
 
 # from src.model import run_fm
-from sklearn.decomposition import TruncatedSVD
 
-import pandas as pd
-import numpy as np
-import category_encoders as ce
+logger = TypeVar("logger")
 
 
 # trainの1つをsvd
-def svd(train_x, test_x, n_components):
+def svd(
+    X_train: np.ndarray, X_test: np.ndarray, n_components: int, anchor: Optional[np.ndarray] = None
+) -> tuple[np.ndarray, ...]:
+    """
+    X_trainを基準にsvdを適用し、X_train, X_test, anchor(あれば)を次元削減する関数
+    """
     svd = TruncatedSVD(n_components=n_components)
-    svd.fit(train_x)
-    svd_train_x = svd.transform(train_x)
-    svd_test_x = svd.transform(test_x)
-    return svd_train_x, svd_test_x
+    svd.fit(X_train)
+    X_train_svd = svd.transform(X_train)
+    X_test_svd = svd.transform(X_test)
+
+    if anchor is not None:
+        anchor_svd = svd.transform(anchor)
+        return X_train_svd, X_test_svd, anchor_svd
+    else:
+        return X_train_svd, X_test_svd
 
 
-def centralize_analysis(config, logger):
+def centralize_analysis(config: Config, logger: logger) -> None:
     """
     集中解析を行う関数
     """
-    # config['input_path']からtrain/testを読み込む
-    train_df = pd.read_csv(config["output_path"] / "train.csv")
-    test_df = pd.read_csv(config["output_path"] / "test.csv")
+    # config['output_path']からtrain/testを読み込む
+    train_df = pd.read_csv(config.output_path / "train.csv")
+    test_df = pd.read_csv(config.output_path / "test.csv")
 
     # 先にsurpriseでの予測を行う
-    rmse = run_surprise(
-        config, train_df, test_df, neightbors=config["neighbors_centlize"]
+    rmse_surprise = run_surprise(
+        dataset=config.dataset, train_df=train_df, test_df=test_df, neightbors=config.neighbors_centlize
     )
-    logger.info("集中解析（協調フィルタリング）のRMSE: {}".format(rmse))
+    logger.info(f"集中解析（協調フィルタリング）のRMSE: {rmse_surprise}")
 
     # ratingをyとし、削除
-    y_train = train_df["rating"]
-    y_test = test_df["rating"]
+    y_train = train_df["rating"].values
+    y_test = test_df["rating"].values
     train_df.drop("rating", axis=1, inplace=True)
     test_df.drop("rating", axis=1, inplace=True)
 
     # 因子分解機のデータ形式に変換
-    target_col = ["uid", "mid"]
-
-    encoder = ce.OneHotEncoder(cols=target_col)
-
+    target_cols = ["uid", "mid"]
+    encoder = ce.OneHotEncoder(cols=target_cols)
     X_train = encoder.fit_transform(train_df)
     X_test = encoder.transform(test_df)
 
     # 集中解析（lightgbm）
-    svd_train_x, svd_test_x = svd(X_train, X_test, config["dim_integrate"])
-    rmse = run_lgbm(config, svd_train_x, y_train, svd_test_x, y_test)
-    logger.info("集中解析（lightgbm）のRMSE: {}".format(rmse))
+    X_train_svd, X_test_svd = svd(X_train=X_train, X_test=X_test, n_components=config.dim_integrate)
+    rmse_lgbm = run_lgbm(X_train=X_train_svd, y_train=y_train, X_test=X_test_svd, y_test=y_test, seed=config.seed)
+    logger.info(f"集中解析（lightgbm）のRMSE: {rmse_lgbm}")
 
     # 集中解析（FM）
     # rmse = run_fm(config, X_train, y_train, X_test, y_test)
@@ -56,23 +71,24 @@ def centralize_analysis(config, logger):
 
 
 def individual_analysis(
-    config, train_x_list, train_y_list, test_x_list, test_y_list, logger
-):
+    Xs_train: list[np.ndarray],
+    ys_train: list[np.ndarray],
+    Xs_test: list[np.ndarray],
+    ys_test: list[np.ndarray],
+    config: Config,
+    logger: logger,
+) -> None:
     """
     個別解析を行う関数
     """
     # 個別解析（lightgbm）
-    loss_list = []
-    for i in range(config["num_institution"]):
-        svd_train_x, svd_test_x = svd(
-            train_x_list[i], test_x_list[i], config["dim_intermediate"]
-        )
-        rmse = run_lgbm(
-            config, svd_train_x, train_y_list[i], svd_test_x, test_y_list[i]
-        )
-        loss_list.append(rmse)
+    losses_lgbm: list[float] = []
+    for X_train, X_test, y_train, y_test in zip(Xs_train, Xs_test, ys_train, ys_test):
+        X_train_svd, X_test_svd = svd(X_train, X_test, config.dim_intermediate)
+        rmse_lgbm = run_lgbm(X_train=X_train_svd, y_train=y_train, X_test=X_test_svd, y_test=y_test)
+        losses_lgbm.append(rmse_lgbm)
 
-    logger.info("個別解析（lightgbm）のRMSE: {}".format(np.mean(loss_list)))
+    logger.info(f"個別解析（lightgbm）のRMSE: {np.mean(losses_lgbm)}")
 
     # 個別解析（FM）
     # loss_list = []
@@ -83,54 +99,46 @@ def individual_analysis(
     # logger.info("個別解析（FM）のRMSE: {}".format(np.mean(loss_list)))
 
     # 個別解析（協調フィルタリング）
-    loss_list = []
-    train_df = pd.read_csv(config["output_path"] / "train.csv")
-    test_df = pd.read_csv(config["output_path"] / "test.csv")
-    for institute in range(config["num_institution"]):
+    losses_sur: list[float] = []
+    train_df = pd.read_csv(config.output_path / "train.csv")
+    test_df = pd.read_csv(config.output_path / "test.csv")
+    for institute in range(config.num_institution):
         # train/testをinstituteごとに抽出し、surpriseでの予測を行う
         train_df_institute = train_df.loc[
-            (train_df["uid"].astype(int) > institute * config["num_institution_user"])
-            & (
-                train_df["uid"].astype(int)
-                <= (institute + 1) * config["num_institution_user"]
-            ),
+            (train_df["uid"].astype(int) > institute * config.num_institution_user)
+            & (train_df["uid"].astype(int) <= (institute + 1) * config.num_institution_user),
             :,
         ]
         test_df_institute = test_df.loc[
-            (test_df["uid"].astype(int) > institute * config["num_institution_user"])
-            & (
-                test_df["uid"].astype(int)
-                <= (institute + 1) * config["num_institution_user"]
-            ),
+            (test_df["uid"].astype(int) > institute * config.num_institution_user)
+            & (test_df["uid"].astype(int) <= (institute + 1) * config.num_institution_user),
             :,
         ]
-        rmse = run_surprise(
-            config,
-            train_df_institute,
-            test_df_institute,
-            neightbors=config["neighbors_individual"],
+        rmse_sur = run_surprise(
+            dataset=config.dataset,
+            train_df=train_df_institute,
+            test_df=test_df_institute,
+            neightbors=config.neighbors_individual,
         )
-        loss_list.append(rmse)
+        losses_sur.append(rmse_sur)
 
-    logger.info("個別解析（協調フィルタリング）のRMSE: {}".format(np.mean(loss_list)))
+    logger.info(f"個別解析（協調フィルタリング）のRMSE: {np.mean(losses_sur)}")
 
 
 def dca_analysis(
-    config,
-    integrate_train_x,
-    integrate_test_x,
-    integrate_train_y,
-    integrate_test_y,
-    logger,
+    X_train_integ: np.ndarray,
+    X_test_integ: np.ndarray,
+    y_train_integ: np.ndarray,
+    y_test_integ: np.ndarray,
+    logger: logger,
+    seed: int = 42,
 ):
     """
     提案手法（データ統合解析）を行う関数
     """
     # 提案手法（lightgbm）
-    rmse = run_lgbm(
-        config, integrate_train_x, integrate_train_y, integrate_test_x, integrate_test_y
-    )
-    logger.info("提案手法（lightgbm）のRMSE: {}".format(rmse))
+    rmse = run_lgbm(X_train=X_train_integ, y_train=y_train_integ, X_test=X_test_integ, y_test=y_test_integ, seed=seed)
+    logger.info(f"提案手法（lightgbm）のRMSE: {rmse}")
 
     # 提案手法（FM）
     # rmse = run_fm(config, integrate_train_x, integrate_train_y, integrate_test_x, integrate_test_y)
